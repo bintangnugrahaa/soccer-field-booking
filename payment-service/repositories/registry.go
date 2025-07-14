2153b4e0f6d5f09a32753f59e9e17c3d8b00b002
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-07-14 08:10:27 +0700

    add payment repository with basic crud methods

---
+++ payment-service/repositories/registry.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	repositories "payment-service/repositories/payment"
+	repositories2 "payment-service/repositories/paymenthistory"
+
+	"gorm.io/gorm"
+)
+
+type Registry struct {
+	db *gorm.DB
+}
+
+type IRepositoryRegistry interface {
+	GetPayment() repositories.IPaymentRepository
+	GetPaymentHistory() repositories2.IPaymentHistoryRepository
+	GetTx() *gorm.DB
+}
+
+func NewRepositoryRegistry(db *gorm.DB) IRepositoryRegistry {
+	return &Registry{db: db}
+}
+
+func (r *Registry) GetPayment() repositories.IPaymentRepository {
+	return repositories.NewPaymentRepository(r.db)
+}
+
+func (r *Registry) GetPaymentHistory() repositories2.IPaymentHistoryRepository {
+	return repositories2.NewPaymentHistoryRepository(r.db)
+}
+
+func (r *Registry) GetTx() *gorm.DB {
+	return r.db
+}
